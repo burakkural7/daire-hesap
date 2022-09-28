e11ce964d4ca6554b707e6e34b452382e931eb62
burakkural7 <113901994+burakkural7@users.noreply.github.com>
2022-09-28 12:00:29 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		double yariCap = 4;
+		double pi = 3.14;
+		double daireAlan = 0;
+		double daireCevre = 0;
+		daireAlan = pi * yariCap * yariCap;
+		System.out.println("Daire'nin Alanı =" + daireAlan);
+		daireCevre = 2 * pi * yariCap;
+		System.out.println("Daire'nin Çevresi =" + daireCevre);
+
+	}
+
+}
